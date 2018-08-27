5f57d6dec99a2e273755c9a3fb1576c6278afa55
Mark McCue <markmccue@comcast.net>
2018-08-26 20:39:27 -0600

    homework

---
+++ app.js
@@ -0,0 +1,59 @@
+
+const inputData = data;
+
+var $tbody = document.querySelector("tbody");
+var $dateInput = document.querySelector("#datetime");
+var $stateInput = document.querySelector("#state")
+var $searchBtn = document.querySelector("#search");
+
+
+$searchBtn.addEventListener("click", handleSearchButtonClick);
+
+var filteredUFO = dataSet;
+
+
+function renderTable() {
+  $tbody.innerHTML = "";
+  console.log(filter)
+
+  for (var i = 0; i < filteredUFO.length; i++) {
+    
+    var sighting = filteredUFO[i];
+    var fields = Object.keys(sighting);
+    var $row = $tbody.insertRow(i);
+    for (var j = 0; j < fields.length; j++) {
+      var field = fields[j];
+      var $cell = $row.insertCell(j);
+      $cell.innerText = sighting[field];
+    }
+  }
+}
+
+function handleSearchButtonClick() {
+  var filterDate = $dateInput.value.trim();
+  var filterState = $stateInput.value.trim().toLowerCase();
+
+  if (filterDate.length != 0){
+    filteredUFO = dataSet.filter(function(sighting) {
+      var sightingDate = sighting.datetime;
+       return sightingDate === filterDate;
+         });
+  }
+  else {
+    filteredUFO = dataSet
+  }
+  if (filterState.length != 0){
+    filteredUFO = filteredUFO.filter(function(sighting) {
+      var sightingState = sighting.state;
+       return sightingState === filterState;
+         });
+  }
+  else {
+    filteredUFO = filteredUFO
+  }
+  renderTable();
+}
+
+
+document.getElementById('filteredUFO').addEventListener('keyup', filterTable, false);    
+
